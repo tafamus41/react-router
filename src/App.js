756551b9d297435dd5df66d88c2d5c20b57a891f
asimov <seyranmustafa4@gmail.com>
2024-09-07 12:21:43 +0300

    navlink

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import React from "react";
 import Home from "./pages/Home";
-import { BrowserRouter, Route,Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
+import MyNavbar from "./components/MyNavbar";
 
 const App = () => {
   return (
     <div>
       <BrowserRouter>
+        <MyNavbar />
         <Routes>
-          <Route path="/" element={<Home />}/>
+          <Route path="/" element={<Home />} />
         </Routes>
       </BrowserRouter>
-      
     </div>
   );
 };
--- src/components/MyNavbar.jsx
+++ src/components/MyNavbar.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from "react";
+import { Container, Nav, Navbar } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
+import logo from "../img/logo.png";
 const MyNavbar = () => {
   return (
-    <div>MyNavbar</div>
-  )
-}
+    <Navbar expand="lg" bg="light">
+      <Container>
+        <Link>
+          <img to="/" src={logo} alt="" />
+        </Link>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <NavLink to="">Home</NavLink>
+            <NavLink to="">Teacher</NavLink>
+            <NavLink to="">Courses</NavLink>
+            <NavLink to="">Contact</NavLink>
+            <NavLink to="">Paths</NavLink>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
 
-export default MyNavbar
+export default MyNavbar;
