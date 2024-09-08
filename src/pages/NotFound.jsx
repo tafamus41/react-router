334e8334f676b8f2523aee32611c754c71f594d4
asimov <seyranmustafa4@gmail.com>
2024-09-09 01:56:24 +0300

    p

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,12 +1,14 @@
 import React from "react";
-import { Container } from "react-bootstrap";
+import Container from "react-bootstrap/Container";
 import notFound from "../img/notFound.jpeg";
+import { useNavigate } from "react-router-dom";
 
 const NotFound = () => {
+  const navigateee=useNavigate()
   return (
     <Container className="text-center">
       <img src={notFound} alt="" width="550px" />
-      <button className="btn btn-info">GO HOME</button>
+      <button onClick={()=>navigateee("/")} className="btn btn-info">GO HOME</button>
     </Container>
   );
 };
