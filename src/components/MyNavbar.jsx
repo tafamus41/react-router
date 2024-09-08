88f55495bfd0799c65103ff95f225e63df2f0612
asimov <seyranmustafa4@gmail.com>
2024-09-09 02:12:35 +0300

    p

--- src/components/MyNavbar.jsx
+++ src/components/MyNavbar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Container, Nav, Navbar } from "react-bootstrap";
-import { Link, NavLink} from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 import logo from "../img/logo.png";
 const MyNavbar = () => {
@@ -13,11 +13,33 @@ const MyNavbar = () => {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <NavLink className="nav-link" to="/">Home</NavLink>
-            <NavLink className="nav-link" to="/teacher">Teacher</NavLink>
-            <NavLink className="nav-link" to="/courses">Courses</NavLink>
-            <NavLink className="nav-link" to="/contact">Contact</NavLink>
-            <NavLink className="nav-link" to="/paths">Paths</NavLink>
+            <NavLink
+              style={(isActive) => ({
+                color: isActive && "red",
+              })}
+              className="nav-link"
+              to="/"
+            >
+              Home
+            </NavLink>
+            <NavLink style={(isActive) => ({
+                fonSize: isActive && "50px",
+              })} className="nav-link" to="/teacher">
+              Teacher
+            </NavLink>
+            <NavLink style={(isActive) => ({
+                border: isActive && "1px solid blue",
+              })} className="nav-link" to="/courses">
+              Courses
+            </NavLink>
+            <NavLink style={(isActive) => ({
+                backgroundColor: isActive && "salmon",
+              })} className="nav-link" to="/contact">
+              Contact
+            </NavLink>
+            <NavLink className="nav-link" to="/paths">
+              Paths
+            </NavLink>
             <Nav.Link href="https://github.com/clarusway">GitHub</Nav.Link>
           </Nav>
         </Navbar.Collapse>
