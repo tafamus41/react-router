e1373608472b7be996cb40330b577be2043aae76
asimov <seyranmustafa4@gmail.com>
2024-09-11 13:18:06 +0300

    p

--- src/pages/TeacherDetails.jsx
+++ src/pages/TeacherDetails.jsx
@@ -4,7 +4,13 @@ import { useLocation } from 'react-router-dom'
 const TeacherDetails = () => {
   const {state:{person}}=useLocation()
   return (
-    <div>TeacherDetails</div>
+    <div className='text-center container mt-4'>
+      <img src={`https://api.dicebear.com/9.x/avataaars/svg?seed=${person.name}`} alt="" width="400px"/>
+      <h3>{person.name}</h3>
+      <h2>{person.email}</h2>
+      <h5>{person.website}</h5>
+      <h5>{person.phone}</h5>
+    </div>
   )
 }
 
