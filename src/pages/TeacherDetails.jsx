386d34b0e96e89572dcbc64eac0664022b417994
asimov <seyranmustafa4@gmail.com>
2024-09-11 13:14:04 +0300

    p

--- src/pages/TeacherDetails.jsx
+++ src/pages/TeacherDetails.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
+import { useLocation } from 'react-router-dom'
 
 const TeacherDetails = () => {
+  const {state:{person}}=useLocation()
   return (
     <div>TeacherDetails</div>
   )
