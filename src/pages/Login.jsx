f2191ddf8abb4eb69c451d00e2dd2a18fc3e60bd
asimov <seyranmustafa4@gmail.com>
2024-09-11 15:11:13 +0300

    p

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,45 +1,54 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
-  const [email,setEmail]=useState("")
-  const [password,setPassword]=useState("")
-  
+  const navigate=useNavigate()
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    localStorage.setItem("EMAIL", JSON.stringify(email))
+    localStorage.setItem("PASSWORD", JSON.stringify(password))
+    navigate("/contact")
+  };
+
   return (
     <div className="container text-center mt-4">
-    <h1 className="display-6 text-danger">LOG IN</h1>
-    <form >
-      <div className="mb-3">
-        <label htmlFor="title" className="form-label">
-          EMAIL
-        </label>
-        <input
-          type="text"
-          className="form-control"
-          id="title"
-          placeholder="Enter your email"
-          required
-         
-        />
-      </div>
-      <div className="mb-3">
-        <label htmlFor="desc" className="form-label">
-         PASSWORD
-        </label>
-        <input
-          type="password"
-          className="form-control"
-          id="desc"
-          placeholder="Enter your password"
-          required
-          
-        />
-      </div>
-      <button type="submit" className="btn btn-danger mb-4">
-        Submit
-      </button>
-    </form>
-  </div>
-  )
-}
+      <h1 className="display-6 text-danger">LOG IN</h1>
+      <form onSubmit={handleSubmit}>
+        <div className="mb-3">
+          <label htmlFor="title" className="form-label">
+            EMAIL
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="title"
+            placeholder="Enter your email"
+            required
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="desc" className="form-label">
+            PASSWORD
+          </label>
+          <input
+            type="password"
+            className="form-control"
+            id="desc"
+            placeholder="Enter your password"
+            required
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <button type="submit" className="btn btn-danger mb-4">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+};
 
-export default Login
+export default Login;
--- src/router/PrivateRouter.jsx
+++ src/router/PrivateRouter.jsx
@@ -4,9 +4,8 @@ import { Navigate, Outlet } from 'react-router-dom'
 
 
 const PrivateRouter = () => {
-    const email="hasmetbabaoglu@gmail.com";
-    const password="1234"
-  return email==="hasmetbabaoglu@gmail.com" && password==="1234"? (<Outlet/>) : (<Navigate to="/"/>)
+   
+  return JSON.parse(localStorage.getItem("EMAIL"))==="tafamus" && JSON.parse(localStorage.getItem("PASSWORD"))==="1234" ? (<Outlet/>) : (<Navigate to="/login"/>)
 }
 
 export default PrivateRouter
